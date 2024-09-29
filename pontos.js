e24c4e56ac0d3bf3337d9a137e3b00c233385dec
Arthur <arhturvitorsilvio@gmail.com>
2024-09-28 22:46:58 -0300

    CEP ADD

---
+++ pontos.js
@@ -0,0 +1,82 @@
+const addressForm = document.querySelector("#address-form");
+const cepInput = document.querySelector("#cep");
+const addressInput = document.querySelector("#address");
+const cityInput = document.querySelector("#city");
+const neighborhoodInput = document.querySelector("#neighborhood");
+const regionInput = document.querySelector("#region");
+const formInputs = document.querySelectorAll("[data-input]");
+const closeButton = document.querySelector("#close-message");
+
+// Validate CEP Input
+cepInput.addEventListener("keypress", (e) => {
+  const onlyNumbers = /[0-9]|\./;
+  const key = String.fromCharCode(e.keyCode);
+
+  console.log(key);
+
+  console.log(onlyNumbers.test(key));
+
+  // allow only numbers
+  if (!onlyNumbers.test(key)) {
+    e.preventDefault();
+    return;
+  }
+});
+
+// Evento to get address
+cepInput.addEventListener("keyup", (e) => {
+  const inputValue = e.target.value;
+
+  //   Check if we have a CEP
+  if (inputValue.length === 8) {
+    getAddress(inputValue);
+  }
+});
+
+// Get address from API
+const getAddress = async (cep) => {
+  toggleLoader();
+
+  cepInput.blur();
+
+  const apiUrl = `https://viacep.com.br/ws/${cep}/json/`;
+
+  const response = await fetch(apiUrl);
+
+  const data = await response.json();
+
+  console.log(data);
+  console.log(formInputs);
+  console.log(data.erro);
+
+  // Activate disabled attribute if form is empty
+  if (addressInput.value === "") {
+    toggleDisabled();
+  }
+
+  addressInput.value = data.logradouro;
+  cityInput.value = data.localidade;
+  neighborhoodInput.value = data.bairro;
+  regionInput.value = data.uf;
+
+  toggleLoader();
+};
+
+// Add or remove disable attribute
+const toggleDisabled = () => {
+  if (regionInput.hasAttribute("disabled")) {
+    formInputs.forEach((input) => {
+      input.removeAttribute("disabled");
+    });
+  } else {
+    formInputs.forEach((input) => {
+      input.setAttribute("disabled", "disabled");
+    });
+  }
+};
+
+// Show or hide loader
+const toggleLoader = () => {
+  const fadeElement = document.querySelector("#fade");
+  fadeElement.classList.toggle("hide");
+};
